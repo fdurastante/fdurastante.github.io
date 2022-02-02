79208c0e84f68f3b512545da51d09ad01feba865
Cirdans-Home <f.durastante@na.iac.cnr.it>
2022-02-02 09:01:10 +0100

    Added Pisa Ph.D. Course

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,3 +108,19 @@ document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});part
   },
   "retina_detect": true
   })},!1);
+
+function readmore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
