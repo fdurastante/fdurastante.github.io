5cd323274d7f05075dcb8846290a819cd01c6b45
Fabio Durastante <fabio.durastante@unipi.it>
2025-02-24 10:17:09 +0100

    Update documentation

--- assets/img/ore.m
+++ assets/img/ore.m
@@ -2,14 +2,17 @@
 
 clear; clc; close all;
 
-ORE = [12,0,0,0;10,42,0,0;10,44,18,52;15,71,10,34;18,62,19,20];
-X = ["2019/20" "2020/21" "2021/22" "2022/23" "2023/24"];
+ORE = [12,0,0,0;10,42,0,0;10,44,18,52;15,71,10,34;18,62,19,20;0,20,56,0];
+X = ["2019/20" "2020/21" "2021/22" "2022/23" "2023/24" "2024/25"];
+Xticklabels = {"2019/20" "2020/21" "2021/22" "2022/23" "2023/24" "2024/25"};
+Xnum = [1:6];
 
 %h = figure(1);
 figure('Position',[899 743 1101 293]);
-bar(X,ORE)
+bar(Xnum,ORE)
 legend({'Other Courses','Bachelor','Master','Ph.D.'}, ...
     'Location','northwest','FontSize',14)
+xticklabels(X)
 
 % Put numbers
 nModel = size(ORE,1);
@@ -23,7 +26,7 @@ for idxModel=1:nCat
         bar_xPos = bar_xPos-xPosAmpl+(idxModel-1)*xPosInc;
     end
     text(bar_xPos,ORE(:,idxModel)',num2str(ORE(:,idxModel),...
-        '%d'),'vert','bottom','horiz','center','FontSize',12); 
+        '%d'),'vert','bottom','horiz','center','FontSize',10); 
 end
 
 % Save Figure to File
